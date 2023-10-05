2d8467bf7113631c0676a48c24e3f0a292f7640a
jungmyungjin <myung2667@gmail.com>
2023-10-06 06:33:58 +0900

    feat: 로그인 버튼, 마이페이지 버튼 구현

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,8 +1,15 @@
 import styles from "./Header.module.scss";
-import { Link } from "react-router-dom";
-// import expandIcon from "assets/icons/expand_more.svg";
+import { ReactElement, useState } from "react";
 
 const Header = () => {
+  const [isLogin, setLogin] = useState(false);
+  const [userName, setUserName] = useState("정명진");
+  const [isClickedProfile, setclickedProfile] = useState(false);
+
+  const clickHandleProfile = () => {
+    setclickedProfile(!isClickedProfile);
+  };
+
   return (
     <div className={styles.layout}>
       <div className={styles.header}>
@@ -21,7 +28,18 @@ const Header = () => {
           </a>
         </div>
         <div className={styles.userInfo}>
-          <div className={styles.auth}>로그인</div>
+          {isLogin === false && <div className={styles.isLogout}>로그인</div>}
+          {isLogin === true && (
+            <div onClick={clickHandleProfile} className={styles.isLogin}>
+              {userName} 님,
+            </div>
+          )}
+          {isLogin && isClickedProfile && (
+            <div className={styles.dropdown}>
+              <div className={styles.dropdownMenu}>마이페이지</div>
+              <div className={styles.dropdownMenu}>로그아웃</div>
+            </div>
+          )}
         </div>
       </div>
       <div className={styles.bottomLine}></div>
